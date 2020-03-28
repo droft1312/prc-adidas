8fea8ecaf868e54ea35707bad597257ecce17c5e
Turar Jumaniyaz <droft1312@gmail.com>
2020-03-28 17:34:51 +0100

    created project

---
+++ cmake-build-debug/CMakeFiles/Adidas.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Adidas.exe"
+  "Adidas.exe.manifest"
+  "Adidas.pdb"
+  "CMakeFiles/Adidas.dir/main.c.obj"
+  "libAdidas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Adidas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Adidas.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Adidas.
+# This may be replaced when dependencies are built.
