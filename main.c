3960c6748ca8272f263e4e6d825fb1d356bf5a7b
Turar Jumaniyaz <droft1312@gmail.com>
2020-03-29 16:21:44 +0200

    managed to make reading work

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+
+    FILE *input = fopen("D:\\Projects\\Adidas\\input.txt", "r");
+    char c;
+
+    if (input == NULL) {
+        printf("Error! File not found.");
+        return -1;
+    }
+
+    fscanf(input, "%c", &c);
+
+    printf("Value of c=%c", c);
+
+    fclose(input);
+
     return 0;
 }
